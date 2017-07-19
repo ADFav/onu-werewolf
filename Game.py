83d94e7e1a15d2ac6b101cc43de0bae3a92e0719
Andrew Favaloro <andrew.favaloro@gmail.com>
2017-07-19 23:12:35 +0000

    Added voting mechanic, including win conditions and how the Hunter works; moved Night Order to be a global variable for the Game object

--- Game.py
+++ Game.py
@@ -5,6 +5,7 @@ from Deck import *
 class Game:
     allGames = {}
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    nightOder = ["Doppleganger","Werewolf","Minion","Mason","Seer","Robber","Troublemaker","Drunk","Insomniac"]
     
     def __init__(self, gameID=None):
         if gameID == None:
@@ -14,7 +15,9 @@ class Game:
         Game.allGames[self.gameID] = self
         self.players = {}
         self.deck = Deck()
-    
+        self.votes = {}
+        self.mostVoted = []
+        
     def __del__(self):
         return 0
         
@@ -46,8 +49,63 @@ class Game:
         if cardName != None:
             map(lambda player: player.initialCard.performNightAction(), self.findCardholders(cardName))
         else:
-            map(lambda role: self.performNightActions(role), ["Doppleganger","Werewolf","Minion","Mason","Seer","Robber","Troublemaker","Drunk","Insomniac"])
+            map(lambda role: self.performNightActions(role), Game.nightOrder)
     
+    def selectPlayers(self, text = "Pick your player", notThisPlayer = []):
+        tmpPlayers = [player for player in self.players if player not in notThisPlayer]
+        for player in tmpPlayers:
+            print "--- " + player
+        choice = raw_input(text + "\n")
+        if choice in tmpPlayers:
+            return self.players[choice]
+        else:
+            print "Not a valid choice!"
+            return self.selectPlayers(text, notThisPlayer)
+    
+    def selectPocket(self, text, notThisCard=[]):
+        tmpPocket = {pos:card for pos,card in self.deck.pocket.iteritems() if pos not in notThisCard}
+        for pos in tmpPocket.keys():
+            print "--- " + pos
+        choice = raw_input(text + "\n")
+        if choice in tmpPocket.keys():
+            return choice
+        else:
+            print "Not a valid choice!"
+            return self.selectPocket(text,notThisCard)
+    
+    def collectVotes(self):
+        for player in self.players.values():
+            player.castVote(self.selectPlayers("Player " + player.name + ", cast your vote.").name)
+    
+    def tallyVotes(self):
+        voteTally = {player:self.votes.values().count(player) for player in self.players}
+        self.mostVoted = [self.players[player] for player in voteTally if voteTally[player] == max(voteTally.values())]
+        if max(voteTally.values()) == 1:
+            self.mostVoted = []
+        
+    def winner(self):
+        self.mostVoted = [player if player.finalCard.name != "Hunter" else self.players[self.votes[player.name]] for player in self.mostVoted]
+        killedWolves   = [player for player in self.mostVoted        if player.finalCard.team == "Werewolf" and player.finalCard.name != "Minion"]
+        wolvesInGame   = [player for player in self.players.values() if player.finalCard.team == "Werewolf" and player.finalCard.name != "Minion"]
+        killedTanners  = [player for player in self.mostVoted        if player.finalCard.team == "Tanner"]
+        killedMinions  = [player for player in self.mostVoted        if player.finalCard.team == "Werewolf" and player.finalCard.name == "Minion"]
+        minionsInGame  = [player for player in self.players.values() if player.finalCard.team == "Werewolf" and player.finalCard.name == "Minion"]
+        result = set([])
+        
+        if killedTanners != []:
+            result.add("Tanner")
+        if self.mostVoted == [] and wolvesInGame == []:
+            result.add("Villagers")
+        if killedWolves != []:
+            result.add("Villagers")
+        if wolvesInGame != [] and killedWolves == [] and killedTanners == []:
+            result.add("Werewolves")
+        if wolvesInGame == [] and minionsInGame != [] and self.mostVoted != [] and killedMinions == [] and killedTanners == []:
+            result.add("Werewolves")
+        
+        return result
+        
+        
     def endGame(self):
         Game.allGames.pop(self.gameID,None)
         for player in self.players:
